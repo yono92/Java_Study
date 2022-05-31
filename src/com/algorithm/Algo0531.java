e9224ca618ed0ab21f67834886a5f426a270ace4
yoonsejong <oxwk123@gmail.com>
2022-05-31 16:13:47 +0900

    Algo first Commit

---
+++ src/com/algorithm/Algo0531.java
@@ -0,0 +1,7 @@
+package com.algorithm;
+
+public class Algo0531 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/algorithm/Main.java
@@ -0,0 +1,9 @@
+package com.algorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("자바 알고리즘 공부 시작");
+    }
+}
