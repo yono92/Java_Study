f9fdb00be98c809388fd458f9c6cade9e2cfe48b
yoonsejong <oxwk123@gmail.com>
2022-06-08 16:10:19 +0900

    Algo0608-2

--- src/com/algorithm/algo/Algo0608.java
+++ src/com/algorithm/algo/Algo0608.java
@@ -1,13 +1,19 @@
 package com.algorithm.algo;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Algo0608 {
     public static void main(String[] args) {
 //        System.out.println(solution(3,5));
 //        System.out.println(solution(3,3));
 //        System.out.println(solution(5,3));
-        int[][] arr1 = {{1, 2}, {2, 4}};
-        int[][] arr2 = {{3, 4}, {5, 6}};
-        System.out.println(solution3(arr1,arr2));
+//        int[][] arr1 = {{1, 2}, {2, 4}};
+//        int[][] arr2 = {{3, 4}, {5, 6}};
+//        System.out.println(solution3(arr1,arr2));
+//        System.out.println(Arrays.toString(solution5(2,5)));
+//        System.out.println(solution6("yoonsejong"));
+        System.out.println(solution7(new int[]{1, 2, 3, 4}));
     }
     public static long solution(int a, int b) {
         long answer = 0;
@@ -78,4 +84,78 @@ public class Algo0608 {
         }
         return answer;
     }
+
+    public static String solution4(String phone_number) {
+        String answer = "";
+
+        // 0 1 0 7 2 2 4 7 6 2 5
+        // 0 1 2 3 4 5 6 7 8 9 10
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length() - 4);
+        return answer;
+    }
+/*
+    var answer = [];
+    let add = x
+    for(let i=0; i<n;i++) {
+        answer.push(x);
+        x += add
+    }
+    return answer;
+ */
+    public static long[] solution5(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n ; i++) {
+            answer[i] = x + x*i;
+        }
+        return answer;
+    }
+    public static String solution6(String s) {
+        String answer = "";
+        if (s.length() % 2 == 0) {
+            for (int i = 0; i < s.length(); i++) {
+                answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+            }
+        } else {
+            answer = s.substring(s.length() / 2 , s.length() / 2 + 1);
+        }
+        return answer;
+    }
+
+    public static double solution7(int[] arr) {
+        double answer = 0;
+        double avg = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+            avg = answer / arr.length;
+
+        }
+        return avg;
+    }
+
+    public static long solution8(long n) {
+        long answer = 0;
+        double result = Math.sqrt(n);
+        if (result % 1 == 0) {
+            answer = (long) Math.pow(result + 1, 2);
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+    /*
+    함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+    예를들어 n이 118372면 873211을 리턴하면 됩니다.
+     */
+    public static long solution9(long n) {
+        long answer = 0;
+        String[] arr = ("" + n).split("");
+        Arrays.sort(arr);
+        Collections.reverse(Arrays.asList(arr));
+        answer = Long.parseLong(String.join("", arr));
+        return answer;
+    }
+
 }
