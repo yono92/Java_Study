8418e2d401ce487030ce00a1407e5b53303edee7
yoonsejong <oxwk123@gmail.com>
2022-06-10 13:24:17 +0900

    Algo0610-2

--- src/com/algorithm/algo/Algo0610.java
+++ src/com/algorithm/algo/Algo0610.java
@@ -2,6 +2,7 @@ package com.algorithm.algo;
 
 public class Algo0610 {
     public static void main(String[] args) {
+        System.out.println(solution3(3, 20, 4));
 
     }
 
@@ -22,5 +23,50 @@ public class Algo0610 {
 //            }
 //        }
         return answer;
+    }
+
+    public static int solution2(int[][] sizes) {
+        int max_w =0;
+        int max_h =0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int temp;
+            for (int j = 0; j < 2; j++) {
+                if (sizes[i][0] < sizes[i][1]) {
+                    temp = sizes[i][0];
+                    sizes[i][0] = sizes[i][1];
+                    sizes[i][1] =temp;
+                }
+                if (max_w < sizes[i][0]) {
+                    max_w = sizes[i][0];
+                }
+                if (max_h < sizes[i][1]) {
+                    max_h = sizes[i][1];
+                }
+
+            }
+        }
+        return max_h*max_w;
+    }
+    /*
+    새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다.
+    이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다.
+    즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
+    놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
+    단, 금액이 부족하지 않으면 0을 return 하세요.
+     */
+    public static long solution3(int price, int money, int count) {
+        long answer = 0;
+        int i = 0;
+//        for (int i = 0; i <=count ; i++) {
+//            answer += price * i;
+//            System.out.println("놀이기구를" + i +"번 타려고합니다.");
+//        }
+        while ( i <= count){
+            System.out.println("놀이기구를" + i +"번 타려고합니다.");
+            answer += price*i;
+            i++;
+        }
+        return answer - money < 0 ? 0 : answer - money ;
     }
 }
